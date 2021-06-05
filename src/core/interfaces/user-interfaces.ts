077da35f0f587c79742401c227246273725059e9
Victor <bearbur@gmail.com>
2021-06-05 08:21:06 +0300

    add auto generate user with admin permissions

--- src/core/interfaces/user-interfaces.ts
+++ src/core/interfaces/user-interfaces.ts
@@ -6,3 +6,12 @@ export interface UserRegReqObjectBody {
 export interface UserRegReqObject {
     body: UserRegReqObjectBody
 }
+
+export interface UserInterface {
+    username: string,
+    password: string,
+    id: string,
+    permission: number,
+    creation_date: number,
+    is_active: boolean
+}
--- src/core/services/admin/db-setting-check.ts
+++ src/core/services/admin/db-setting-check.ts
@@ -2,21 +2,42 @@
 import { loggerCreator } from '../logger/logger';
 import User from '../../models/user/user-model';
 import { userPermissions, ADMIN } from '../../constants/user-permission-levels';
+import {UserInterface} from '../../interfaces/user-interfaces';
+import { ARRAY_ZERO_LENGTH } from '../../constants/utils-constants';
+import { generateHashPassword } from '../../utils/hash-passwords-utils';
 
 const dataBaseSettingsCheck = () => {
 
 
     loggerCreator.info('Data base async check on servive start');
 
-    User.find({permission: userPermissions[ADMIN].level},(err:Error,docs:[])=>{
+    User.find({permission: userPermissions[ADMIN].level}, (err: Error, docs: UserInterface[] | [])=>{
         if(err){
-            loggerCreator.error(err);
+            loggerCreator.error('DB error: ', err.toString());
 
             return;
         }
-        loggerCreator.info(`Admin users at db: ${docs.length}`);
-        /* todo on docs.length === 0 create default admin user at data base */
-    })
+        if(docs.length === ARRAY_ZERO_LENGTH){
+            loggerCreator.warn(`Admin users at db: ${docs.length}`);
+            const password = '_PASSWORD123';
+            
+            generateHashPassword(password).then((hashedPassword: string)=>{
+                const newAdminUser = new User({username: '_ADMIN', password: hashedPassword, permission: userPermissions[ADMIN].level  });
+
+                newAdminUser.save().then(()=>{
+                    loggerCreator.warn(`Admin user (default) was created.`);
+                }) .catch((errorNewUser:Error) => {
+                    
+                    loggerCreator.error('New user at db error ', errorNewUser.toString());
+                   
+                });
+                }
+            ).catch((errorHash:Error)=>{
+                loggerCreator.error('Hash generate error ', errorHash);
+            })
+
+         }
+        })
 
 }
 
